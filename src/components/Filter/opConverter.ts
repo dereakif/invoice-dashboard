0d07612b58e62f56a5a7e23503649dcab61fe7c9
Anonymous <>
2022-01-19 21:44:11 +0300

    refactor: opConverter

--- src/components/Filter/opConverter.ts
+++ src/components/Filter/opConverter.ts
@@ -1,32 +1,40 @@
-export default function (type: string) {
+export default function (op: string, type: string) {
   let text = "";
-  switch (type) {
+  let sign = "";
+  switch (op) {
     case "eq":
       text = "esittir";
+      sign = "=";
       break;
 
     case "neq":
       text = "esit degildir";
+      sign = "!=";
       break;
 
     case "gt":
       text = "buyuktur";
+      sign = ">";
       break;
 
     case "lt":
       text = "kucuktur";
+      sign = "<";
       break;
 
     case "ge":
       text = "buyuk esittir";
+      sign = ">=";
       break;
 
     case "le":
       text = "kucuk esittir";
+      sign = "<=";
       break;
 
     default:
       break;
   }
-  return text;
+  if (type === "asText") return text;
+  if (type === "asSign") return sign;
 }
