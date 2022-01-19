231387909207a2b88b2c12b8408512be1640366e
Anonymous <>
2022-01-19 22:24:05 +0300

    add: sorter to columns

--- src/components/TableContainer/index.tsx
+++ src/components/TableContainer/index.tsx
@@ -5,6 +5,7 @@ import { initialTag, TagT } from "../../interfaces/filter.interfaces";
 import { formatDate } from "../../modules/date";
 import Filter from "../Filter";
 import opConverter from "../Filter/opConverter";
+import { DataItemT, SchemaItemT } from "../Filter/schema";
 const { Search } = Input;
 interface Props {}
 
@@ -64,29 +65,36 @@ const TableContainer = (props: Props) => {
       dataIndex: "service",
       key: "service",
       render: (text: string) => <a>{text}</a>,
+      sorter: (a: DataItemT, b: DataItemT) =>
+        a.service.localeCompare(b.service),
     },
     {
       title: "Fatura Numarasi",
       dataIndex: "invoice_id",
       key: "invoice_id",
+      sorter: (a: DataItemT, b: DataItemT) => a.invoice_id - b.invoice_id,
     },
     {
       title: "Tarih",
       dataIndex: "date",
       key: "date",
       render: (date: Date) => renderDate(date),
+      sorter: (a: DataItemT, b: DataItemT) =>
+        a.date.getTime() - b.date.getTime(),
     },
     {
       title: "Tutar",
       dataIndex: "amount",
       key: "amount",
       render: (amount: number) => <span>${amount}</span>,
+      sorter: (a: DataItemT, b: DataItemT) => a.amount - b.amount,
     },
     {
       title: "Durum",
       key: "status",
       dataIndex: "status",
       render: (status: string) => renderStatus(status),
+      sorter: (a: DataItemT, b: DataItemT) => a.status.localeCompare(b.status),
     },
     {
       title: "",
@@ -104,7 +112,7 @@ const TableContainer = (props: Props) => {
       id: 1,
       service: "DMP",
       invoice_id: 521,
-      date: new Date(),
+      date: new Date(2018, 2, 2),
       amount: 196.15,
       status: "notPaid",
     },
@@ -112,7 +120,7 @@ const TableContainer = (props: Props) => {
       id: 2,
       service: "DMP",
       invoice_id: 514,
-      date: new Date(),
+      date: new Date(2018, 2, 1),
       amount: 196.15,
       status: "pending",
     },
@@ -120,7 +128,7 @@ const TableContainer = (props: Props) => {
       id: 3,
       service: "DMP",
       invoice_id: 561,
-      date: new Date(),
+      date: new Date(2018, 1, 2),
       amount: 196.15,
       status: "pending",
     },
@@ -128,7 +136,7 @@ const TableContainer = (props: Props) => {
       id: 4,
       service: "DMP",
       invoice_id: 666,
-      date: new Date(),
+      date: new Date(2018, 4, 2),
       amount: 196.15,
       status: "pending",
     },
@@ -136,7 +144,7 @@ const TableContainer = (props: Props) => {
       id: 5,
       service: "SSP",
       invoice_id: 261,
-      date: new Date(),
+      date: new Date(2018, 2, 5),
       amount: 196.15,
       status: "pending",
     },
@@ -144,7 +152,7 @@ const TableContainer = (props: Props) => {
       id: 6,
       service: "DMP",
       invoice_id: 511,
-      date: new Date(),
+      date: new Date(2019, 2, 2),
       amount: 196.15,
       status: "pending",
     },
@@ -152,7 +160,7 @@ const TableContainer = (props: Props) => {
       id: 7,
       service: "SSP",
       invoice_id: 461,
-      date: new Date(),
+      date: new Date(2020, 7, 3),
       amount: 196.15,
       status: "paid",
     },
