0d21cda7b8b482202bebe8c592a5e9942b8f4781
Anonymous <>
2022-01-20 04:22:11 +0300

    create: RowT

--- src/interfaces/filter.interfaces.ts
+++ src/interfaces/filter.interfaces.ts
@@ -6,3 +6,12 @@ export interface TagT {
 }
 
 export const initialTag: TagT = { id: "", name: "", value: "", op: "" };
+
+export interface RowT {
+  id: number;
+  service: string;
+  invoice_id: number;
+  date: Date;
+  amount: number;
+  status: string;
+}
