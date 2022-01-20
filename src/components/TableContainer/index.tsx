b260e7b1b9fcd81885ad59371a468e720abab43d
Anonymous <>
2022-01-20 04:28:18 +0300

    implement: Query

--- src/components/TableContainer/index.tsx
+++ src/components/TableContainer/index.tsx
@@ -1,23 +1,43 @@
 import { DownloadOutlined, FilterOutlined } from "@ant-design/icons";
 import { Button, Col, Input, Popover, Row, Space, Table, Tag } from "antd";
 import { useEffect, useState } from "react";
-import { initialTag, TagT } from "../../interfaces/filter.interfaces";
+import { initialTag, RowT, TagT } from "../../interfaces/filter.interfaces";
 import { formatDate } from "../../modules/date";
 import Filter from "../Filter";
+import { columns } from "../Filter/columns";
+import initialRows from "../Filter/initialRows";
+import nameConverter from "../Filter/nameConverter";
 import opConverter from "../Filter/opConverter";
-import { DataItemT, SchemaItemT } from "../Filter/schema";
+import { Query } from "../Filter/query";
+import statusConverter from "../Filter/statusConverter";
+
 const { Search } = Input;
 interface Props {}
-
 const TableContainer = (props: Props) => {
   const [visible, setVisible] = useState(false);
-
+  const [rows, setRows] = useState<RowT[]>(initialRows);
   const [filters, setFilters] = useState<TagT[]>([]);
   const [tag, setTag] = useState<TagT>(initialTag);
 
   useEffect(() => {
-    console.log({ tag });
-  }, [tag]);
+    if (filters.length > 0) {
+      let filteredRows: RowT[] = [];
+      filters.forEach((condition) => {
+        let filteredItem = initialRows.filter((row) => {
+          return Query(
+            row[condition.name.toLowerCase() as keyof RowT],
+            condition.name,
+            condition.value,
+            condition.op
+          );
+        });
+        filteredRows = filteredRows.concat(filteredItem);
+      });
+      setRows(filteredRows);
+    } else {
+      setRows(initialRows);
+    }
+  }, [filters]);
 
   const onSearch = (value: string) => console.log(value);
   const handleCloseTag = (id: string) => {
@@ -28,143 +48,7 @@ const TableContainer = (props: Props) => {
       return current;
     });
   };
-  const renderStatus = (status: string) => {
-    let color = "";
-    let text = "";
-    switch (status) {
-      case "paid":
-        color = "green";
-        text = "Odendi";
-        break;
-      case "pending":
-        color = "orange";
-        text = "Bekliyor";
-        break;
-      case "notPaid":
-        color = "volcano";
-        text = "Odenmedi";
-        break;
 
-      default:
-        break;
-    }
-    return (
-      <Tag color={color} key={status}>
-        {text}
-      </Tag>
-    );
-  };
-
-  const renderDate = (date: Date) => (
-    <span>{formatDate(date.toISOString())}</span>
-  );
-
-  const columns = [
-    {
-      title: "Servis Adi",
-      dataIndex: "service",
-      key: "service",
-      render: (text: string) => <a>{text}</a>,
-      sorter: (a: DataItemT, b: DataItemT) =>
-        a.service.localeCompare(b.service),
-    },
-    {
-      title: "Fatura Numarasi",
-      dataIndex: "invoice_id",
-      key: "invoice_id",
-      sorter: (a: DataItemT, b: DataItemT) => a.invoice_id - b.invoice_id,
-    },
-    {
-      title: "Tarih",
-      dataIndex: "date",
-      key: "date",
-      render: (date: Date) => renderDate(date),
-      sorter: (a: DataItemT, b: DataItemT) =>
-        a.date.getTime() - b.date.getTime(),
-    },
-    {
-      title: "Tutar",
-      dataIndex: "amount",
-      key: "amount",
-      render: (amount: number) => <span>${amount}</span>,
-      sorter: (a: DataItemT, b: DataItemT) => a.amount - b.amount,
-    },
-    {
-      title: "Durum",
-      key: "status",
-      dataIndex: "status",
-      render: (status: string) => renderStatus(status),
-      sorter: (a: DataItemT, b: DataItemT) => a.status.localeCompare(b.status),
-    },
-    {
-      title: "",
-      key: "action",
-      render: () => (
-        <Space size="middle">
-          <a>Goster</a>
-        </Space>
-      ),
-    },
-  ];
-
-  const data = [
-    {
-      id: 1,
-      service: "DMP",
-      invoice_id: 521,
-      date: new Date(2018, 2, 2),
-      amount: 196.15,
-      status: "notPaid",
-    },
-    {
-      id: 2,
-      service: "DMP",
-      invoice_id: 514,
-      date: new Date(2018, 2, 1),
-      amount: 196.15,
-      status: "pending",
-    },
-    {
-      id: 3,
-      service: "DMP",
-      invoice_id: 561,
-      date: new Date(2018, 1, 2),
-      amount: 196.15,
-      status: "pending",
-    },
-    {
-      id: 4,
-      service: "DMP",
-      invoice_id: 666,
-      date: new Date(2018, 4, 2),
-      amount: 196.15,
-      status: "pending",
-    },
-    {
-      id: 5,
-      service: "SSP",
-      invoice_id: 261,
-      date: new Date(2018, 2, 5),
-      amount: 196.15,
-      status: "pending",
-    },
-    {
-      id: 6,
-      service: "DMP",
-      invoice_id: 511,
-      date: new Date(2019, 2, 2),
-      amount: 196.15,
-      status: "pending",
-    },
-    {
-      id: 7,
-      service: "SSP",
-      invoice_id: 461,
-      date: new Date(2020, 7, 3),
-      amount: 196.15,
-      status: "paid",
-    },
-  ];
   return (
     <Row>
       <Col span={24}>
@@ -208,21 +92,23 @@ const TableContainer = (props: Props) => {
                 onClose={() => handleCloseTag(tag.id)}
                 key={tag.id}
               >
+                {`${nameConverter(tag.name)} ${opConverter(tag.op, "asSign")} `}
                 {tag.name === "Date"
-                  ? `${tag.name} ${opConverter(tag.op, "asSign")} ${formatDate(
-                      tag.value
-                    )}`
-                  : `${tag.name} ${opConverter(
-                      tag.op,
-                      "asSign"
-                    )} ${tag.value.toString()}`}
+                  ? `${formatDate(tag.value)}`
+                  : tag.name === "Status"
+                  ? ` ${statusConverter(tag.value)}`
+                  : ` ${tag.value.toString()}`}
               </Tag>
             ))}
           </Col>
         </Row>
         <Row>
           <Col span={24}>
-            <Table columns={columns} dataSource={data} />
+            <Table
+              rowSelection={{ type: "checkbox" }}
+              columns={columns}
+              dataSource={rows}
+            />
           </Col>
         </Row>
       </Col>
